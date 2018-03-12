0bfcc80cb6abe4ff21c2aceb5986dbb16b0f0826
dimdym <dimdym@gmail.com>
2018-03-12 13:44:28 -0500

    added function to read the labels and to map patients to labels

--- n2b2.py
+++ n2b2.py
@@ -5,6 +5,39 @@ sys.dont_write_bytecode = True
 import xml.etree.ElementTree as et
 import os.path, glob, string
 
+def map_patients_to_labels(xml_dir, category):
+  """Get a patient number to label mapping"""
+
+  # key: patient num, value: met or not met
+  doc2labels = {}
+
+  for xml_file in os.listdir(xml_dir):
+    patient_num = xml_file.split('.')[0]
+    xml_file_path = os.path.join(xml_dir, xml_file)
+    tree = et.parse(xml_file_path)
+
+    for tags in tree.iter(category):
+      doc2labels[patient_num] = tags.attrib['met']
+
+  return doc2labels
+
+def get_category_names(xml_dir):
+  """Get a list of selection criteria"""
+
+  categories = []
+
+  # take categories from the first file
+  for xml_file in os.listdir(xml_dir):
+    xml_file_path = os.path.join(xml_dir, xml_file)
+    tree = et.parse(xml_file_path)
+
+    for tags in tree.iter('TAGS'):
+      for category in tags:
+        categories.append(category.tag)
+    break
+
+  print categories
+
 def write_notes_to_files(xml_dir, out_dir):
   """Extract notes from xml and write to files"""
 
@@ -26,4 +59,4 @@ if __name__ == "__main__":
   xml_dir = '/Users/Dima/Loyola/Data/Trials/Xml/'
   out_dir = '/Users/Dima/Loyola/Data/Trials/Text/'
 
-  write_notes_to_files(xml_dir, out_dir)
+  map_patients_to_labels(xml_dir, 'MAKES-DECISIONS')
